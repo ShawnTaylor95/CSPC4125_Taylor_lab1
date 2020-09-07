4f0d039d6078cd6c3da8d233741671c11a39c6da
ShawnTaylor95 <Taylor_Shawn2@Columbusstate.edu>
2020-09-07 21:48:54 +0000

    attempt to do add js code

--- to_do.js
+++ to_do.js
@@ -1,3 +1,7 @@
-<script>
-    
-</script>
+function addFunction(){
+  document.getElementsByClassName("btn btn-success")
+  var text = document.getElementsByClassName("form-control").value;
+  <ul class="list-group-flush"></ul>
+  document.getElementsByClassName("list-group-flush").appendChild(li);
+}
+
