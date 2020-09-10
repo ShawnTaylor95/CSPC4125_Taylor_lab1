e0b96d0d7bb31f8e9da55262fd6c5ef3af700f9a
ShawnTaylor95 <Taylor_Shawn2@Columbusstate.edu>
2020-09-10 17:18:58 +0000

    Add and Delete buttons for JS and CSS changes

--- to_do.js
+++ to_do.js
@@ -1,7 +1,18 @@
-function addFunction(){
-  document.getElementsByClassName("btn btn-success")
-  var text = document.getElementsByClassName("form-control").value;
-  <ul class="list-group-flush"></ul>
-  document.getElementsByClassName("list-group-flush").appendChild(li);
+function addFunction() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("userInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+    document.getElementById("myList").appendChild(li);
 }
 
+function delFunction() {
+  var list = document.getElementById("myList");
+  list.removeChild(list.childNodes[0]);
+}
+
+function resetFunction(){
+    var list = [Toothpaste, Shampoo, Grapes, Bananas]
+    x = document.getElementById("myList")
+    x.innerHTML = list;
+}
